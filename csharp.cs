24535f091609a9c2cef148bbd1302124d8c10d7c
Anas AP <62279876+AnasAP@users.noreply.github.com>
2021-04-13 21:28:16 +0530

    Create csharp.cs

---
+++ csharp.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+namespace Arduinoldr
+{
+class Program
+{
+static void Main(string[] args)
+{
+SerialPort myport = new SerialPort();
+myport.BaudRate = 9600;
+myport.PortName = “COM6”;
+myport.Open();
+while (true)
+{
+String data = myport.ReadLine();
+Console.WriteLine(data);
+}
+}
+}
+}
+
